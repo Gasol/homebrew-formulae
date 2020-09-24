be9eb8ea48b3ac0f16bc57f7b4fb64fd6e870666
Gasol Wu <gasol.wu@gmail.com>
2020-09-24 10:58:57 +0800

    Use head blocks instead of devel blocks
    Because devel blocks has been deprecated since Homebrew 2.4.0.
    Please see Homebrew/brew#7688 for details.
    
    Fix #2

--- aglio.rb
+++ aglio.rb
@@ -1,17 +1,17 @@
-require "language/node"
-
 class Aglio < Formula
+  require "language/node"
+
   desc "API Blueprint renderer"
   homepage "https://github.com/danielgtaylor/aglio/"
+  license "MIT"
 
-  depends_on "node"
-
-  devel do
+  head do
     url "https://github.com/Gasol/aglio/archive/333da5.tar.gz"
     version "333da5"
     sha256 "7952eccf8ee89a2a05e6a89160d855f377777132a5637782db369b84e4a35d64"
-
   end
+
+  depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.local_npm_install_args
